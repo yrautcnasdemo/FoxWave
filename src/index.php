874c1f822687f60891a9734929ca8629089cec3c
yrautcnasdemo <yrautcnas@msn.com>
2024-08-26 15:22:02 +0200

    Mise en page storyboard & texte

---
+++ src/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="fonts.css">
+    <title>FoxGlitch your library</title>
+</head>
+<body class="homepage">
+    <div class="blackfilter">
+        <header>
+            <a class="btn-login" href="#"><button>Log/Register</button></a>
+        </header>
+
+        <main>
+            <section class="presentation">
+                <article>
+                    <h1>Welcome to FoxGlitch !</h1>
+                    <p>Your personal library dedicated to manga and comic book enthusiasts. Here, you can create, organize, and share your collection in a simple and intuitive way. Whether you're a seasoned collector or a casual reader, FoxGlitch helps you keep track of your readings, discover new series, and share your favorites with friends. Dive into the world of graphic stories and turn your collection into a true digital treasure. Explore, organize, and let your passion for manga and comics shine!</p>
+                </article>
+                <div>
+                    <div class="storyboards">
+                        <img class="storyboard1" src="assets/images/backgrounds/homepage_storyboard1bis.jpg" height="550px" alt="">
+                        <img class="storyboard2" src="assets/images/backgrounds/homepage_storyboard2.jpg" height="200px" alt="">
+                        <img class="homepage-img1" src="assets/images/backgrounds/homepage_img1.jpg" width="400px" alt="">
+                        <img class="homepage-img2" src="assets/images/backgrounds/homepage_img2.jpg" width="400px" alt="">
+                        <img class="homepage-img3" src="assets/images/backgrounds/homepage_img3.jpg" width="400px" alt="">
+                    </div>
+                </div>
+            </section>
+        </main>
+
+        <footer>
+            <p>© Copyright 2024 De Meyer Guilain. Tous droits réservés.</p>
+            <p>Plan du site | Mentions légales | Politique de confidentialité</p>
+            <p>Special thanks</p>
+        </footer>
+    </div>
+</body>
+</html>
