0629c24f926b8e6fdb92e6fd5db75aecb52907a2
yrautcnasdemo <yrautcnas@msn.com>
2024-09-02 16:58:59 +0200

    friend list page

---
+++ src/friendslist.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="fonts.css">
+    <title>userName</title>
+</head>
+<body >
+    <main class="user-body">
+        <section class="user-space">
+            <div class="user-banner">
+                <img src="assets/images/profiles/profiles_banner/cyberpunk-night-city2.jpg" alt="">
+            </div>
+
+
+            <div class="user-panel">
+                <div class="user-profile-friendlist">
+                    <div class="user-pics">
+                        <div class="user-info">
+                            <span class="user-name">Judy Alvarez</span>
+                            <span class="user-title">Space-Child</span>
+                        </div>
+                        <img src="assets/images/profiles/profiles_pictures/cute-anime-girl-R.jpg" width="300px" height="500px" alt="profile-pics">
+                            <div class="user-btn-panel">
+                                <a href=""><button>Add books</button></a>
+                                <a href="friendslist.php"><img src="assets/images/icones/Friend List(1).png" width="35px" alt=""></a>
+                                <a href=""><img src="assets/images/icones/Wishlist.png" width="35px" alt=""></a>
+                                <a href=""><img src="assets/images/icones/GearWhite-Small-icon01.png" width="35px" alt=""></a>
+                            </div>                    
+                    </div>
+                    <div class="friendlist-tools">
+                        <!-- <div class="loop-grid-fl">
+                            <video class="fl-movie" autoplay loop>
+                                <source src="assets/images/backgrounds/Video/Cj4Q.mp4" type="video/mp4">
+                            </video>
+                        </div> -->
+                        <div>
+                            <h3>Friends List</h3>
+                            <div div class="fl-disp">
+                                <input type="text" placeholder="Add-Friend"><a href=""><img src="assets/images/icones/Retrowave/addFriend.png" alt=""></a>
+                            </div>
+                        </div>
+                        <div>
+                            <h3>Filter</h3>
+                            <input type="text" placeholder="Pseudo...">
+                            <div class="fl-disp">
+                                <input type="text" placeholder="Select a group..."><a href=""><img src="assets/images/icones/Retrowave/selecteur.png" alt=""></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="user-social-media">
+                        <a href="#"><img src="assets/images/icones/Mediainstagram.png" width="35px" alt="instagram-icon"></a>
+                        <a href="#"><img src="assets/images/icones/Mediafacebook.png" width="35px" alt="facebook-icon"></a>
+                        <a href="#"><img src="assets/images/icones/X-twitter.png" width="35px" alt="twitter-icon"></a>
+                        <a href="#"><img src="assets/images/icones/steam.png" width="35px" alt="steam-icon"></a>
+                        <a href="#"><img src="assets/images/icones/twitch.webp" width="35px" alt="twitch-icon"></a>
+                        <a href="#"><img src="assets/images/icones/SL.png" width="35px" alt="SL-icon"></a>
+                    </div>
+                </div>
+
+
+                <div class="friendslist-board">
+                    <div class="bloc-fl-unique">
+                        <img class="banner-fl-sup" src="assets/images/profiles/banner-fl-sup.png" alt="banner-flsup">
+                        <img class="banner-fl-down" src="assets/images/profiles/banner-fl-down.png" alt="banner-fldown">
+                        <img class="pp-friend" src="assets/images/profiles/profiles_pictures/catwim.jpg" alt="profile-pic-friend">
+                        <h2>Furry-Kurry</h2>
+                        <span>Space-kitten</span>
+                    </div>
+                    <div class="bloc-fl-unique">
+                        <img class="banner-fl-sup" src="assets/images/profiles/banner-fl-sup.png" alt="banner-flsup">
+                        <img class="banner-fl-down" src="assets/images/profiles/banner-fl-down.png" alt="banner-fldown">
+                        <img class="pp-friend" src="assets/images/profiles/profiles_pictures/cutefox.jpg" alt="profile-pic-friend">
+                        <h2>Yrautcnas</h2>
+                        <span>Fox you</span>
+                    </div>
+                    <div class="bloc-fl-unique">
+                        <img class="banner-fl-sup" src="assets/images/profiles/banner-fl-sup.png" alt="banner-flsup">
+                        <img class="banner-fl-down" src="assets/images/profiles/banner-fl-down.png" alt="banner-fldown">
+                        <img class="pp-friend" src="assets/images/profiles/profiles_pictures/dj-monkey.jpg" alt="profile-pic-friend">
+                        <h2>Donky-Monk</h2>
+                        <span>Hoist our colors</span>
+                    </div>
+                    <div class="bloc-fl-unique">
+                        <img class="banner-fl-sup" src="assets/images/profiles/banner-fl-sup.png" alt="banner-flsup">
+                        <img class="banner-fl-down" src="assets/images/profiles/banner-fl-down.png" alt="banner-fldown">
+                        <img class="pp-friend" src="assets/images/profiles/profiles_pictures/eyes.jpg" alt="profile-pic-friend">
+                        <h2>Little-E</h2>
+                        <span>Not crazy, just passionate!</span>
+                    </div>
+                </div>
+
+
+            
+
+
+        </section>
+    </main>
+    <footer>
+            <p>© Copyright 2024 De Meyer Guilain. Tous droits réservés.</p>
+            <p>Plan du site | Mentions légales | Politique de confidentialité</p>
+            <p>Special thanks</p>
+    </footer>
+</body>
+<script src="script.js"></script>
+</html>
--- src/user.php
+++ src/user.php
@@ -25,7 +25,7 @@
                         <img src="assets/images/profiles/profiles_pictures/cute-anime-girl-R.jpg" width="300px" height="500px" alt="profile-pics">
                             <div class="user-btn-panel">
                                 <a href=""><button>Add books</button></a>
-                                <a href=""><img src="assets/images/icones/Friend List(1).png" width="35px" alt=""></a>
+                                <a href="friendslist.php"><img src="assets/images/icones/Friend List(1).png" width="35px" alt=""></a>
                                 <a href=""><img src="assets/images/icones/Wishlist.png" width="35px" alt=""></a>
                                 <a href=""><img src="assets/images/icones/GearWhite-Small-icon01.png" width="35px" alt=""></a>
                             </div>                    
@@ -35,12 +35,12 @@
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas id ex  felis. Ut sapien orci, dictum sed massa eu, euismod tincidunt tellus.  Curabitur eget ex cursus, hendrerit dui sed, auctor metus. Aliquam  blandit a nisi a auctor. In at ex nec nunc maximus consequat in ac  magna. Nunc diam magna, eleifend sed fermentum ut, iaculis id ante.  Mauris vel quam non mauris maximus fermentum. Quisque porta et mauris  quis sollicitudin. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas id ex  felis. Ut sapien orci, dictum sed massa eu, euismod tincidunt tellus.  Curabitur eget ex cursus, hendrerit dui sed, auctor metus. Aliquam  blandit a nisi a auctor. In at ex nec nunc maximus consequat in ac  magna. Nunc diam magna, eleifend sed fermentum ut, iaculis id ante.  Mauris vel quam non mauris maximus fermentum. Quisque porta et mauris  quis sollicitudin.</p>
                     </div>
                     <div class="user-social-media">
-                        <a href=""><img src="assets/images/icones/Mediainstagram.png" width="35px" alt="instagram-icon"></a>
-                        <a href=""><img src="assets/images/icones/Mediafacebook.png" width="35px" alt="facebook-icon"></a>
-                        <a href=""><img src="assets/images/icones/X-twitter.png" width="35px" alt="twitter-icon"></a>
-                        <a href=""><img src="assets/images/icones/steam.png" width="35px" alt="steam-icon"></a>
-                        <a href=""><img src="assets/images/icones/twitch.webp" width="35px" alt="twitch-icon"></a>
-                        <a href=""><img src="assets/images/icones/SL.png" width="35px" alt="SL-icon"></a>
+                        <a href="#"><img src="assets/images/icones/Mediainstagram.png" width="35px" alt="instagram-icon"></a>
+                        <a href="#"><img src="assets/images/icones/Mediafacebook.png" width="35px" alt="facebook-icon"></a>
+                        <a href="#"><img src="assets/images/icones/X-twitter.png" width="35px" alt="twitter-icon"></a>
+                        <a href="#"><img src="assets/images/icones/steam.png" width="35px" alt="steam-icon"></a>
+                        <a href="#"><img src="assets/images/icones/twitch.webp" width="35px" alt="twitch-icon"></a>
+                        <a href="#"><img src="assets/images/icones/SL.png" width="35px" alt="SL-icon"></a>
                     </div>
                 </div>
 
