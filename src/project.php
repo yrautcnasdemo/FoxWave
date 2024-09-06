5e97ad0897e42a1a3f62ff99ba8cf9836aeae97d
yrautcnasdemo <yrautcnas@msn.com>
2024-09-06 15:18:57 +0200

    this project intro

--- src/project.php
+++ src/project.php
@@ -9,9 +9,46 @@
 </head>
 <body>
 
-<p>FoxGlitch est mon dernier projet réalisé en formation, nous avions une total liberté, j'ai donc décidé de développer un site de bibliothèque Manga/Comics pour mes amis et moi même</p>
-
+<p>FoxGlitch est mon dernier projet réalisé en formation, et comme nous avions une total liberté, j'ai décidé de développer un site de bibliothèque Manga/Comics pour mes amis et moi même</p>
+<p>Mes objectifs</p>
+<p>
+    1-Pouvoir créer sa propre liste de mangas/comics
+    2-pouvoir la mettre a jour facilement
+    0-Créer un site relativement intuitif 
+    3-Modifier et customiser son profile 
+    4-Laisser une place pour faire evoluer le site
+    5-Integrer un system de friend list
+    6-Integrer un system de Wish list
+    7-Integrer un system de chat
+    </p>
 
 
 </body>
 </html>
+
+
+
+
+
+
+<!-- <p>
+01joker – Alors comme ca tu veux en savoir plus sur ce projet, batou ?
+02Batman- Non…
+03joker- Mais tu as cliker, donc… ca t’interesse forcement… héhéhé
+03Batman- Arrete Joker, abandonne, quelque soit ton plan je le réduirai a néant... 
+04joker – Vraiment ? Alors prepare toi pour un monologue que te laissera sans voix batou !
+05Batman - *surpris/appeuré* Non pas un monologue !
+06joker- monologue présentation du site 01
+07Batman&superman – Clark Non vas t’en, c’est un monologue interminable, tu ne tiendra pas ,et puis ca n’a ni queue ni tête...
+08joker – fin monologue, Avec ce site je semerais le chaos dans gotham  
+09batman enervé- ,,,
+10Joker- Et voila comment je vais détruire Gotham Batou ! 
+11batman-Ton plan est completement stupide joker… 
+12Joker- Maintenant que tu le dit…. 
+</p> -->
+
+
+<!-- Ah, Batman, toujours à traîner dans l’ombre avec ton sens moral bien aiguisé… Mais laisse-moi te parler de quelque chose d’un peu plus… amusant, oui ? Imagine un endroit, une petite cachette secrète – non pas pour tes gadgets ou tes batarangs, non, non, non – mais pour… des mangas et des comics. Oh oui, mon cher Chevalier Noir, un site où tu peux collectionner toutes tes précieuses histoires de justiciers masqués et de méchants tordus.
+Ça s’appelle… Ah, quel joli nom aurais-je pu trouver pour ça ? Peu importe, c’est notre petite bibliothèque ! Là, tu entres le titre de tes comics, tu enregistres chaque volume avec précision – comme toi avec tes preuves. Ensuite, tu sélectionnes ton précieux statut : est-ce que ta collection est complète, ou peut-être qu’elle est encore en train de croître… comme une belle petite folie, hmmm ?! Et tu ne fais pas tout ça seul, oh non, mon cher Batounet ! Tu peux ajouter… des amis ! Oui, des amis ! Et partager ta précieuse liste avec eux. Qui sait, peut-être qu’un jour tu seras aussi apprécié pour tes lectures que pour ton sens de la justice défaillant, héhé !
+Oh, mais attention, Batman… une collection partagée peut révéler plus que tu ne le crois. Alors, tu joins la danse ou tu restes dans ta Batcave, à relire tes vieux exemplaires de Detective Comics ?
+Allez, fais-moi plaisir, rejoins la fête ! -->
