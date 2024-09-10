f580e0dc43db9ff75e1aa05f7481953086952407
yrautcnasdemo <yrautcnas@msn.com>
2024-09-10 11:13:01 +0200

    modal

--- src/index.php
+++ src/index.php
@@ -35,20 +35,40 @@
             </section>
         </main>
 
-        <section>
-            <div class="modal">
+            <section class="modal">
                 <article class="register-modal">
+                    <div class="banner-line">
+                        <button class="close-modal">X</button>
+                    </div>
+                    <h2>Login - Register Station</h2>
                     <form action="">
-                        <h2>Login - Register</h2>
-                        <button>X</button>
-                        <span>Mail</span><input type="mail">
-                        <span>Password</span><input type="password">
-                        <p>Don't have an account ? <a href="">register here</a></p>
-                        <button>Login</button>
+                        <div class="input-wrapper">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+                        </svg>
+
+                            <input type="email" class="login-input" placeholder="YourMail@Adress.com" require>
+                        </div>
+                        <div class="input-wrapper">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1 1 21.75 8.25Z" />
+                        </svg>
+
+                            <input type="password" class="login-input">
+                        </div>
+                        <div class="rem-for">
+                            <label for="remember-me">
+                                <input type="checkbox" id="remember-me">
+                                Remember me
+                            </label>
+                            <p><a href="">Forgot password ?</a></p>
+                        </div>
+
+                        <button class="btn-modal">Login</button>
+                            <p>Not registered ? <a class="create-acc" href="">Creat account</a></p>
                     </form>
                 </article>
-            </div>
-        </section>
+            </section>
 
         <footer>
             <p>© Copyright 2024 De Meyer Guilain. Tous droits réservés.</p>
