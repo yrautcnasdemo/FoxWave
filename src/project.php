b3ea38dfa29fe867b6df2c69049ca956e3b94b42
yrautcnasdemo <yrautcnas@msn.com>
2024-09-13 14:15:24 +0200

    the joker case speech

--- src/project.php
+++ src/project.php
@@ -21,9 +21,7 @@
             </div>
             <div class="case-speech-2"><p>Wow, Batou... t'as surpasse Mister J !</p></div>
             <img class="case-3" src="assets/images/Project-Storyboard/07-Batman-superman.png" alt="">
-            <!-- JOKER START -->
-            <!-- <img class="case-4" src="assets/images/Project-Storyboard/06-joker.png" alt=""> -->
-            <!-- JOKER END -->
+
             <div class="case-4-frame"> <!-- DIV - CARDRE -->
                 <div class="div-bg-joker"> <!-- DIV - OVER HIDDEN FOR THE BACKGROUND + scale 1.2 -->
                     <img class="bg-joker" src="assets/images/Project-Storyboard/multiple-joker1-0.png" alt="bgjoker"> <!-- IMG - THE BACKGROUND -->
@@ -31,11 +29,12 @@
                 <div class="div-the-joker"> <!-- DIV NOT OVER HIDDEN FOR THE JOKER -->
                     <img class="the-joker" src="assets/images/Project-Storyboard/multiple-joker1-1.png"  alt=""> <!-- THE JOKER WITHOUT BACKGROUND -->
                 </div>
+                <img class="speech-joker" src="assets/images/Project-Storyboard/speech1-joker.png"  alt=""> <!-- THE JOKER SPEECH CASE -->
             </div>
 
 
             <figcaption class="story-case">
-                <p>Welcome to my project</p>
+                <p>Welcome <br> to my project</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam rerum accusamus nulla, et placeat in, inventore, quis sit quisquam illo ullam obcaecati reprehenderit cum! Saepe numquam fugit odit! Nulla veritatis, quasi cupiditate error, deserunt iure, sint non neque nemo nam incidunt? Iste eos earum, nisi rem soluta tempora repellendus iure quod praesentium nulla reprehenderit numquam expedita hic facere possimus, ratione iusto nesciunt sed dolores error impedit obcaecati accusantium id. Maiores ullam voluptatum eos, fuga sapiente veritatis rerum quia dignissimos dolor harum, neque inventore officia provident, animi ut iste quibusdam iusto rem quisquam delectus accusantium quis exercitationem quasi. Totam culpa illo, maxime nostrum architecto, vitae, dolores velit perspiciatis et autem eos ducimus cum libero incidunt. Inventore pariatur porro consequatur non in maiores nemo, saepe dicta nihil itaque ex maxime voluptas nostrum sapiente tenetur modi molestiae ducimus, dolor nobis.</p>
             </figcaption>
             <img class="next-icon" src="assets/images/Project-Storyboard/next-icon.png" alt="">
